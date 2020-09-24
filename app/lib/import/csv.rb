eabb9ca55ff4eda2c1ce869a71122367fd24a188
Maxime Souillat <maxime@beaucouplus.com>
2020-09-24 18:43:15 +0200

    evolved CSV class to handle working with upsert_all

--- app/lib/import/csv.rb
+++ app/lib/import/csv.rb
@@ -1,16 +1,31 @@
 require 'csv'
+require 'active_support/core_ext/string/inflections.rb'
 
 module Import
   class Csv
-    attr_reader :filename, :rows
-    def initialize(filename:)
-      @filename = filename
+    attr_reader :file_path, :filename, :rows
+    def initialize(file_path:)
+      @file_path = file_path
+      @filename = File.basename(file_path, '.csv')
       @rows = []
     end
 
     def import
-      CSV.foreach(filename, headers: true) { |row| @rows << row.to_h }
-      rows
+      current_time = Time.current
+      CSV.foreach(file_path, headers: true) do |row|
+        @rows << row.to_h.merge(created_at: current_time, updated_at: current_time)
+      end
+      self
+    end
+
+    def save
+      return self if rows.empty?
+
+      tablename.constantize.upsert_all(rows)
+    end
+
+    def tablename
+      filename.singularize.camelize
     end
   end
 end
--- spec/import/csv_spec.rb
+++ spec/import/csv_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe Import::Csv, type: :csv do
-  PATH = 'spec/import/generated.csv'.freeze
+  PATH = 'spec/import/buildings.csv'.freeze
 
-  subject { Import::Csv.new(filename: generated_csv.path) }
+  subject { Import::Csv.new(file_path: generated_csv.path) }
 
   after(:each) { File.delete(PATH) if File.exist?(PATH) }
 
@@ -17,10 +17,47 @@ describe Import::Csv, type: :csv do
 
   describe '#import' do
     it 'imports csv data as an array of hashes' do
-      import = subject.import
-      expect(import).to be_kind_of(Array)
-      expect(import.first).to be_kind_of(Hash)
-      expect(import.first['zip_code']).to eq('75009')
+      rows = subject.import.rows
+      expect(rows).to be_kind_of(Array)
+      expect(rows.first).to be_kind_of(Hash)
+      expect(rows.first['zip_code']).to eq('75009')
+    end
+  end
+
+  describe 'filename' do
+    before { subject.import }
+
+    it 'returns filename' do
+      expect(subject.filename).to eq 'buildings'
+    end
+  end
+
+  describe '#tablename' do
+    before { subject.import }
+
+    it 'imports csv data as an array of hashes' do
+      expect(subject.tablename).to eq('Building')
+    end
+  end
+
+  describe '#save' do
+    before { subject.import }
+    it 'populates table with imported records' do
+      subject.save
+
+      expect(Building.count).to eq(2)
+      expect(Building.first.zip_code).to eq('75009')
+    end
+
+    context 'when record already exists with same reference' do
+      before do
+        Building.create!(subject.rows.find { |row| row['reference'] == '1' }.merge({ 'zip_code': '75020' }))
+      end
+      it 'updates record with new data' do
+        expect(Building.find_by(reference: '1')['zip_code']).to eq('75020')
+        subject.save
+        expect(Building.find_by(reference: '1')['zip_code']).to eq('75009')
+      end
     end
   end
 end
