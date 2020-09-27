d26f5d7a11ca5fd21ea309ae52589becdaacd48a
Maxime Souillat <maxime@beaucouplus.com>
2020-09-27 14:30:04 +0200

    import > save works as expected + tests

--- app/lib/import/csv.rb
+++ app/lib/import/csv.rb
@@ -3,7 +3,7 @@ require 'active_support/core_ext/string/inflections.rb'
 
 module Import
   class Csv
-    attr_reader :file_path, :filename, :rows
+    attr_reader :file_path, :filename, :rows, :records
     def initialize(file_path:)
       @file_path = file_path
       @filename = File.basename(file_path, '.csv')
@@ -11,9 +11,8 @@ module Import
     end
 
     def import
-      current_time = Time.current
       CSV.foreach(file_path, headers: true) do |row|
-        @rows << row.to_h.merge(created_at: current_time, updated_at: current_time)
+        @rows << row.to_h
       end
       self
     end
@@ -21,7 +20,31 @@ module Import
     def save
       return self if rows.empty?
 
-      tablename.constantize.upsert_all(rows)
+      current_table = tablename.constantize
+      with_profile = current_table.with_profiles
+      rows.each do |row|
+        record = with_profile.find_or_create_by(reference: row['reference'])
+        profile_attributes = row.reject { |k, _v| k == 'reference' }
+
+        forbidden_overwrite_keys = current_table::NO_OVERWRITE.map(&:to_s)
+        forbidden_overwrite_values = row.slice(*forbidden_overwrite_keys)
+
+        where_clause = forbidden_overwrite_keys.map { |item| "#{item} = :#{item}" }.join(' or ')
+        found_previous = record.profiles.where(where_clause, forbidden_overwrite_values.symbolize_keys)
+        if found_previous.any?
+          rejected_fields = found_previous.map do |item|
+            item.attributes.select { |_k, v| v.in?(forbidden_overwrite_values.values) }
+          end.reduce(&:update).keys
+
+          last_profile_attributes = record.current_profile.attributes.reject { |k, _v| k.in?(['id', "#{filename.singularize}_id", 'created_at', 'updated_at']) }
+
+          profile_attributes = last_profile_attributes.merge(profile_attributes.reject { |k, _v| k.in?(rejected_fields) })
+        end
+        profile = record.profiles.build(profile_attributes)
+        profile.save
+        record.update(current_profile_id: profile.id)
+      end
+      true
     end
 
     def tablename
--- spec/import/csv_spec.rb
+++ spec/import/csv_spec.rb
@@ -1,63 +1,170 @@
 require 'rails_helper'
 
 describe Import::Csv, type: :csv do
-  PATH = 'spec/import/buildings.csv'.freeze
-
   subject { Import::Csv.new(file_path: generated_csv.path) }
 
-  after(:each) { File.delete(PATH) if File.exist?(PATH) }
+  after(:each) { File.delete(path) if File.exist?(path) }
 
-  let(:generated_csv) do
-    CSV.open(PATH, 'wb') do |csv|
-      csv << %w[reference address zip_code city country manager_name]
-      csv << ['1', '10 Rue La bruyère', '75009', 'Paris', 'France', 'Martin Faure']
-      csv << ['2', '40 Rue René Clair', '75018', 'Paris', 'France', 'Martin Faure']
-    end
-  end
+  describe 'for buildings' do
+    let(:path) { 'spec/import/buildings.csv' }
 
-  describe '#import' do
-    it 'imports csv data as an array of hashes' do
-      rows = subject.import.rows
-      expect(rows).to be_kind_of(Array)
-      expect(rows.first).to be_kind_of(Hash)
-      expect(rows.first['zip_code']).to eq('75009')
-    end
-  end
-
-  describe 'filename' do
-    before { subject.import }
-
-    it 'returns filename' do
-      expect(subject.filename).to eq 'buildings'
-    end
-  end
-
-  describe '#tablename' do
-    before { subject.import }
-
-    it 'imports csv data as an array of hashes' do
-      expect(subject.tablename).to eq('Building')
-    end
-  end
-
-  describe '#save' do
-    before { subject.import }
-    it 'populates table with imported records' do
-      subject.save
-
-      expect(Building.count).to eq(2)
-      expect(Building.first.zip_code).to eq('75009')
-    end
-
-    context 'when record already exists with same reference' do
-      before do
-        Building.create!(subject.rows.find { |row| row['reference'] == '1' }.merge({ 'zip_code': '75020' }))
-      end
-      it 'updates record with new data' do
-        expect(Building.find_by(reference: '1')['zip_code']).to eq('75020')
-        subject.save
-        expect(Building.find_by(reference: '1')['zip_code']).to eq('75009')
+    let(:generated_csv) do
+      CSV.open(path, 'wb') do |csv|
+        csv << %w[reference address zip_code city country manager_name]
+        csv << ['1', '10 Rue La bruyère', '75009', 'Paris', 'France', 'Martin Faure']
+        csv << ['2', '40 Rue René Clair', '75018', 'Paris', 'France', 'Martin Faure']
       end
     end
+
+    describe '#import' do
+      it 'imports csv data as an array of hashes' do
+        rows = subject.import.rows
+        expect(rows).to be_kind_of(Array)
+        expect(rows.first).to be_kind_of(Hash)
+        expect(rows.first['zip_code']).to eq('75009')
+      end
+    end
+
+    describe 'filename' do
+      before { subject.import }
+
+      it 'returns filename' do
+        expect(subject.filename).to eq 'buildings'
+      end
+    end
+
+    describe '#tablename' do
+      before { subject.import }
+
+      it 'imports csv data as an array of hashes' do
+        expect(subject.tablename).to eq('Building')
+      end
+    end
+
+    describe '#save' do
+      context 'when there is no previous record' do
+        before { subject.import }
+
+        it 'populates table with imported records' do
+          subject.save
+
+          expect(Building.count).to eq(2)
+          expect(Building.first.zip_code).to eq('75009')
+        end
+      end
+
+      context 'when building already has a profile and we ask to update authorized fields' do
+        before { subject.import }
+
+        before do
+          create_building_profile(reference: '1', manager_name: 'Martin Faure', zip_code: '75020')
+        end
+
+        it 'updates record with new data' do
+          expect(Building.find_by(reference: '1').zip_code).to eq('75020')
+          subject.save
+          expect(Building.find_by(reference: '1').reload.zip_code).to eq('75009')
+        end
+      end
+
+      context 'when building already has a profile and we ask to update a protected field' do
+        before { subject.import }
+
+        before do
+          create_building_profile(reference: '1', manager_name: 'Martin Faure', zip_code: '75020')
+          create_building_profile(reference: '1', manager_name: 'Pepito Rodriguez', zip_code: '75011')
+        end
+
+        let(:building_one) { Building.find_by(reference: '1') }
+
+        it 'does not update protected field but updates other fields' do
+          expect(building_one.manager_name).to eq('Pepito Rodriguez')
+          expect(building_one.zip_code).to eq('75011')
+
+          subject.save
+
+          expect(building_one.reload.manager_name).to eq('Pepito Rodriguez')
+          expect(building_one.reload.zip_code).to eq('75009')
+        end
+      end
+    end
+  end
+
+  describe 'for people' do
+    let(:path) { 'spec/import/people.csv' }
+
+    let(:generated_csv) do
+      CSV.open(path, 'wb') do |csv|
+        csv << %w[reference firstname lastname home_phone_number mobile_phone_number email address]
+        csv << ['1', 'Henri', 'Dupont', '0123456789', '0623456789', 'h.dupont@gmail.com', '10 Rue La bruyère']
+        csv << ['2', 'Jean', 'Durand', '0123336789', '0663456789', 'jdurand@gmail.com', '40 Rue René Clair']
+      end
+    end
+
+    describe '#save' do
+      let(:person_one) { Person.find_by(reference: '1') }
+
+      context 'when there is no previous record' do
+        before { subject.import }
+
+        it 'populates table with imported records' do
+          subject.save
+
+          expect(Person.count).to eq(2)
+          expect(Person.first.email).to eq('h.dupont@gmail.com')
+        end
+      end
+
+      context 'when person already has a profile and the csv contains new data' do
+        before { subject.import }
+
+        before do
+          create_person_profile(reference: '1', email: 'h.dupont@gmail.com', lastname: 'Snow', home_phone_number: nil)
+        end
+
+        it 'updates record with new data' do
+          expect(person_one.lastname).to eq('Snow')
+          subject.save
+          expect(person_one.reload.lastname).to eq('Dupont')
+        end
+      end
+
+      context 'when person already has a profile and we ask to update a protected field' do
+        before { subject.import }
+
+        before do
+          create_person_profile(reference: '1', email: 'h.dupont@gmail.com', lastname: 'Snow', home_phone_number: nil)
+          create_person_profile(reference: '1', email: 'buzz@leclair.com', lastname: 'Gigantic', home_phone_number: '0123456789')
+          create_person_profile(reference: '1', email: 'mickey@mouse.com', lastname: 'Mice', home_phone_number: '0875446799')
+        end
+
+        it 'updates record with new data' do
+          expect(person_one.lastname).to eq('Mice')
+          expect(person_one.email).to eq('mickey@mouse.com')
+          expect(person_one.home_phone_number).to eq('0875446799')
+
+          subject.save
+
+          expect(person_one.reload.lastname).to eq('Dupont')
+          expect(person_one.reload.email).not_to eq('h.dupont@gmail.com')
+          expect(person_one.reload.home_phone_number).not_to eq('0123456789')
+        end
+      end
+    end
+  end
+
+  def create_building_profile(reference:, manager_name:, zip_code:)
+    with_building = Building.last ? :with_existing_building : nil
+    building_profile = create(:building_profile, with_building, manager_name: manager_name, zip_code: zip_code)
+    building = building_profile.building
+    building.update(reference: reference, current_profile_id: building_profile.id)
+  end
+
+  def create_person_profile(reference:, email:, lastname:, home_phone_number:)
+    with_person = Person.last ? :with_existing_person : nil
+    with_phone_number = home_phone_number ? { home_phone_number: home_phone_number } : {}
+    person_profile = create(:person_profile, with_person, { email: email, lastname: lastname }.merge(with_phone_number))
+    person = person_profile.person
+    person.update(reference: reference, current_profile_id: person_profile.id)
   end
 end
